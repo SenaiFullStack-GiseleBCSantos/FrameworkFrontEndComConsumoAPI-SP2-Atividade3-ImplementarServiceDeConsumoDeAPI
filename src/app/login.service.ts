19daa129c88057dce8d2b70cb1fd6a2291eb4e35
GiseleBCSantos <catce.2023111mtds0035@aluno.ifpi.edu.br>
2024-01-31 21:56:35 -0300

    ajustes finais component Restrito com login e logout

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -10,22 +10,22 @@ export class LoginService {
 
   constructor() { }
 
+
   login(usuario:string, senha:string):void{
-    if(usuario=="aluno" && senha == "1234"){
-      localStorage.setItem('token','qwer1234');
+    if(usuario=='aluno' && senha =='1234'){
+      localStorage.setItem('token', 'qwer1234');
       this.mostraMenu.next(false);
-    }else{
-      this.mostraMenu.next(true);
-      window.location.reload();
+    }
+    else{
+      this.mostraMenu.next(true)
     }
   }
 
   setMostraMenu(valor: boolean){
-    this.mostraMenu.next(valor);
+    this.mostraMenu.next(valor)
   }
 
   getMostraMenu(){
     return this.mostraMenu.asObservable();
   }
-
 }
